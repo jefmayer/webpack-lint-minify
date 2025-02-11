c59eaf12c49e9cd65420360ae437162b359a17f5
Jef Mayer <jef.a.mayer@gmail.com>
2025-02-11 10:12:17 -0700

    dynamically adding /dist dirs and file clean-up to webpack.confg

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,50 @@
 const config = require('./project-config');
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+
+class CreateDirectoryPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.beforeRun.tapAsync('CreateDirectoryPlugin', (compilation, callback) => {
+      this.options.directories.forEach((dir) => {
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir, { recursive: true });
+        }
+      });
+      callback();
+    });
+  }
+}
+
+class RemoveFilePlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.done.tap('CustomTaskPlugin', () => {
+      const files = this.options.files;
+      Object.keys(files).forEach(key => {
+        try {
+          const file = files[key].match(/\/([^\/]+)\.scss$/)[1];
+          const assetPath = path.resolve(__dirname, `assets/dist/css/${file}.js`);
+          if (fs.existsSync(assetPath)) {
+            fs.unlinkSync(assetPath);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      })
+    });
+  }
+}
 
 module.exports = [
   {
@@ -43,6 +84,12 @@ module.exports = [
     },
     plugins: [
       new ESLintPlugin({ extensions: ['js', 'jsx'] }),
+      new CreateDirectoryPlugin({
+        directories: [
+          path.resolve(__dirname, 'assets/dist/css'),
+          path.resolve(__dirname, 'assets/dist/js')
+        ]
+      })
     ],
     devtool: 'source-map',
   },
@@ -63,7 +110,7 @@ module.exports = [
           test: /\.s[ac]ss$/i,
           use: [
             MiniCssExtractPlugin.loader,
-            { loader: "css-loader", options: { sourceMap: true } },
+            { loader: "css-loader", options: { sourceMap: true, url: false } },
             'postcss-loader',
             { loader: "sass-loader", options: { sourceMap: true } },
           ],
@@ -73,6 +120,7 @@ module.exports = [
     plugins: [
       new MiniCssExtractPlugin({ filename: '[name].min.css' }),
       new StylelintPlugin({ files: 'assets/src/scss/**/*.{scss,sass,css}' }),
+      new RemoveFilePlugin({ files: config.paths.css }), 
     ],
     devtool: 'source-map',
   }
